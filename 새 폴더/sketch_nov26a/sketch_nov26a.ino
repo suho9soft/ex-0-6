d5dc3fe18c81f8cdaddc65d3ff50a9625a32cc61
suho9soft <94418346+suho9soft@users.noreply.github.com>
2024-11-28 14:21:40 +0900

    Update sketch_nov26a.ino

--- 새 폴더/sketch_nov26a/sketch_nov26a.ino
+++ 새 폴더/sketch_nov26a/sketch_nov26a.ino
@@ -1,7 +1,11 @@
-#define LED1 2
-#define LED2 3
-#define LED3 4
-#define LED4 5
+#define LED1 26
+#define LED2 28
+#define LED3 30
+#define LED4 32
+#define LED5 34
+#define LED6 36
+#define LED7 38
+#define LED8 40
 
 void setup() {
     Serial.begin(9600);
@@ -9,6 +13,10 @@ void setup() {
     pinMode(LED2, OUTPUT);
     pinMode(LED3, OUTPUT);
     pinMode(LED4, OUTPUT);
+    pinMode(LED5, OUTPUT);
+    pinMode(LED6, OUTPUT);
+    pinMode(LED7, OUTPUT);
+    pinMode(LED8, OUTPUT);
 }
 
 void loop() {
@@ -39,6 +47,30 @@ void loop() {
                 break;
             case '8': // LED4 OFF
                 digitalWrite(LED4, LOW);
+                break;
+            case '9': // LED5 ON
+                digitalWrite(LED5, HIGH);
+                break;
+            case 'A': // LED5 OFF
+                digitalWrite(LED5, LOW);
+                break;
+            case 'B': // LED6 ON
+                digitalWrite(LED6, HIGH);
+                break;
+            case 'C': // LED6 OFF
+                digitalWrite(LED6, LOW);
+                break;
+            case 'D': // LED7 ON
+                digitalWrite(LED7, HIGH);
+                break;
+            case 'E': // LED7 OFF
+                digitalWrite(LED7, LOW);
+                break;
+            case 'F': // LED8 ON
+                digitalWrite(LED8, HIGH);
+                break;
+            case 'G': // LED8 OFF
+                digitalWrite(LED8, LOW);
                 break;
             default:
                 break;
